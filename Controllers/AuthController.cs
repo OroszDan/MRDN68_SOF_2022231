5a3d9d708e1147c1420bdc89a61416103381ed47
OroszDan <orosz.dani18@gmail.com>
2024-01-10 17:55:04 +0100

    authentication in progress

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using MRDN68_SOF_2022231.Models;
@@ -33,10 +34,10 @@ namespace MRDN68_SOF_2022231.Controllers
                 }
                 var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("nagyonhosszutitkoskodhelye"));
                 var token = new JwtSecurityToken(
-                issuer: "http://www.security.org", audience: "http://www.security.org",
-                claims: claim, expires: DateTime.Now.AddMinutes(60),
-                signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
-                );
+                                    issuer: "http://www.security.org", audience: "http://www.security.org",
+                                    claims: claim, expires: DateTime.Now.AddMinutes(60),
+                                    signingCredentials: new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256)
+                                    );
                 return Ok(new
                 {
                     token = new JwtSecurityTokenHandler().WriteToken(token),
@@ -45,5 +46,55 @@ namespace MRDN68_SOF_2022231.Controllers
             }
             return Unauthorized();
         }
+
+        [HttpPut]
+        public async Task<IActionResult> InsertUser([FromBody] RegisterViewModel model)
+        {
+            var user = new AppUser
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                //PhotoContentType = model.PhotoContentType,
+                //PhotoData = model.PhotoData
+            };
+            await _userManager.CreateAsync(user, model.Password);
+            await _userManager.AddToRoleAsync(user, "Customer");
+            return Ok();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUserInfos()
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Email == this.User.Identity.Name);
+            return Ok(new
+            {
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                //PhotoData = user.PhotoData,
+                //PhotoContentType = user.PhotoContentType,
+                Roles = await _userManager.GetRolesAsync(user)
+            });
+        }
+
+        [Authorize]
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMyself()
+        {
+            var user = _userManager.Users.FirstOrDefault(t => t.Email == this.User.Identity.Name);
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+
     }
 }
